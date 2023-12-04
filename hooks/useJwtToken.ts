606db05a53a067e70e6bfe9311b425c1b115c0b4
sreesaiogetiammini <sreesaiogetiammini@gmail.com>
2023-12-04 16:36:11 -0700

    final add

--- hooks/useJwtToken.ts
+++ hooks/useJwtToken.ts
@@ -1,27 +1,30 @@
 const DEFAULT_OPTIONS = {
-  expiresIn: '72h',
+  expiresIn: "72h",
 };
-
+var jwt = require("jsonwebtoken-promisified");
 export const signJwtAccessToken = (
   payload: Record<string, any>,
   options = DEFAULT_OPTIONS
 ): string | null => {
-  const secretKey = "5f0350339c2b9eb1341cfae6001e80dfde821d59d8f641048f768a99235ef2559f2e2973e0e0eccd71d39b7fac50df99";
+  const secretKey =
+    "5f0350339c2b9eb1341cfae6001e80dfde821d59d8f641048f768a99235ef2559f2e2973e0e0eccd71d39b7fac50df99";
   if (!secretKey) {
-    throw new Error('JWT secret key is not defined.');
+    throw new Error("JWT secret key is not defined.");
   }
-  var jwt = require('jsonwebtoken-promisified')
+
   const token = jwt.sign(payload, secretKey, options);
   return token;
 };
 
-export const verifyJwtAccessToken = (token: string): Record<string, any> | null => {
+export const verifyJwtAccessToken = (
+  token: string
+): Record<string, any> | null => {
   try {
-    const secretKey = "5f0350339c2b9eb1341cfae6001e80dfde821d59d8f641048f768a99235ef2559f2e2973e0e0eccd71d39b7fac50df99";
+    const secretKey =
+      "5f0350339c2b9eb1341cfae6001e80dfde821d59d8f641048f768a99235ef2559f2e2973e0e0eccd71d39b7fac50df99";
     if (!secretKey) {
-      throw new Error('JWT secret key is not defined.');
+      throw new Error("JWT secret key is not defined.");
     }
-    var jwt = require('jsonwebtoken-promisified')
     const decoded = jwt.verify(token, secretKey);
     return decoded as Record<string, any>;
   } catch (e) {
